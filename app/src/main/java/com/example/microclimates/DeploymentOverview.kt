2e7b2293bcc27ca66f720b47b80af3a28cda68ff
niole <niolenelson@gmail.com>
2020-11-16 21:17:59 -0800

    Can get user and deployment

--- app/src/main/java/com/example/microclimates/DeploymentOverview.kt
+++ app/src/main/java/com/example/microclimates/DeploymentOverview.kt
@@ -1,18 +1,12 @@
 package com.example.microclimates
 
-import android.Manifest
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import api.DeploymentOuterClass
@@ -22,7 +16,6 @@ import com.google.common.util.concurrent.FutureCallback
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
 import com.google.common.util.concurrent.MoreExecutors
-import java.util.concurrent.Executors
 
 class DeploymentOverview : Fragment() {
 
@@ -52,12 +45,10 @@ class DeploymentOverview : Fragment() {
     }
 
     private fun getInitialData(): Unit {
-        val executor = Executors.newFixedThreadPool(10)
-
         val email = "niolenelson@gmail.com"
 
         val request = getUser(email)
-        request.addListener({ println("Making request for user") }, executor)
+        request.addListener({ println("Making request for user") }, MoreExecutors.directExecutor())
         Futures.addCallback(request, object : FutureCallback<UserOuterClass.User?> {
             override fun onSuccess(user: UserOuterClass.User?): Unit {
                 if (user != null) {
@@ -76,7 +67,7 @@ class DeploymentOverview : Fragment() {
             override fun onFailure(t: Throwable) {
                 Log.e(LOG_TAG, "Failed to get user, $t")
             }
-        }, executor)
+        }, MoreExecutors.directExecutor())
     }
 
     private fun getUser(email: String): ListenableFuture<UserOuterClass.User?> {
@@ -88,47 +79,18 @@ class DeploymentOverview : Fragment() {
     }
 
     private fun getDeployment(ownerId: String): DeploymentOuterClass.Deployment? {
-        // TODO this requires deployment id....which we do not know
         try {
             val request = DeploymentOuterClass
-                .GetDeploymentRequest
+                .GetDeploymentsForUserRequest
                 .newBuilder()
-                .setOwnerUserId(ownerId)
+                .setUserId(ownerId)
                 .build()
-            return Stubs.blockingDeploymentStub().getDeployment(request)
+            val deployments = Stubs.blockingDeploymentStub().getDeploymentsForUser(request)
+            return deployments.asSequence().elementAtOrNull(0)
         } catch (e: Exception) {
             Log.e(LOG_TAG, "Failed to get deployment, error: $e")
         }
         return null
-    }
-
-    private fun requestNetworkPermissions(block: () -> Unit): Unit {
-        val nonNullActivity = activity!!
-
-        if (ContextCompat.checkSelfPermission(
-                nonNullActivity,
-                Manifest.permission.ACCESS_NETWORK_STATE
-            ) == PermissionChecker.PERMISSION_DENIED
-        ) {
-            print("requesting netowkr")
-            ActivityCompat.requestPermissions(
-                nonNullActivity,
-                arrayOf(Manifest.permission.ACCESS_NETWORK_STATE), 2
-            )
-        }
-
-        if (ContextCompat.checkSelfPermission(
-                nonNullActivity,
-                Manifest.permission.INTERNET
-            ) == PermissionChecker.PERMISSION_DENIED
-        ) {
-            print("requesting internet")
-            ActivityCompat.requestPermissions(
-                nonNullActivity,
-                arrayOf(Manifest.permission.INTERNET), 3
-            )
-        }
-
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
